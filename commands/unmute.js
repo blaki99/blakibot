3ea0dc0290957899be8fd9c12625173d05e91f0c
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-07-11 00:26:20 +0200

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -5,7 +5,7 @@ module.exports.run = async (blaki, message, args) => {
   if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("❌ **Nie można wykonać akcji** ❌");
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
   if(!rMember) return message.reply("❌ **Nie znaleziono użytkownika** ❌");
-  let role = message.guild.roles.find(role => role.name === "MUTED");
+  let role = message.guild.roles.find(role => role.name === "MUTED 🔇");
   let gRole = message.guild.roles.find(role => role.name === "VERIFIED 🔶️");
 
   if(!rMember.roles.has(role.id)) return message.reply("❌ **Ten użytkownik nie jest wyciszony** ❌");
