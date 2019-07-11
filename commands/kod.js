e58151b4360df400d6ecf18537e0544f9ea24946
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-07-11 18:37:02 +0200

    Update kod.js

--- commands/kod.js
+++ commands/kod.js
@@ -1,11 +1,9 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (blaki, message, args) => {
-    let sicon = message.guild.iconURL;
     let serverembed = new Discord.RichEmbed()
-    .setDescription("KOD W SKLEPIE")
+    .setDescription(`**KOD W SKLEPIE!**`)
     .setColor("#15f153")
-    .addField("Nazwa Serwera", message.guild.name)
     .setImage('https://i.imgur.com/jj2M5mv.png')
     .setTimestamp(message.createdAt)
     .setFooter('Zostałeś wyciszony!', 'https://i.imgur.com/7xm6SSI.png');
