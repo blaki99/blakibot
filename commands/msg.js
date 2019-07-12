feadd82ea037f49ade69638a0ae1cd6d2f310db5
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-07-12 02:28:32 +0200

    Create msg.js

---
+++ commands/msg.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (blaki, message, args) => {
+
+    let Szefuncio = message.guild.roles.find("name", "HOST 🔌");
+
+    let msg = args.slice(0).join(' ')
+    const check = '577121399043522560'
+
+    if(!message.member.roles.has(Szefuncio.id)) return message.reply("oops");
+    if(!args[0]) return message.channel.send("❌ _Wprowadź prawidłowe wartości, **!late hasło tryb**_ ❌").then(() =>
+    {
+        message.channel.send("❌ _**Hasło musi posiadać dokładnie 7 znaków!**_ ❌");
+    })
+    message.delete();
+    let msgEmbed = new Discord.RichEmbed()
+    .setColor("#000000")
+    .setDescription(`**POWIADOMIENIE OD:** ${message.author}`)
+    .setTitle("__**INFORMACJA**__")
+    .addField(```${msg}**```)
+    .setTimestamp(message.createdAt)
+    .setFooter("Kliknij reakcje jeśli przeczytałeś", "https://i.imgur.com/7xm6SSI.png");
+    message.channel.send(msgEmbed).then(function (message) {
+        message.react(check)
+    })
+}
+
+module.exports.help = {
+    name: "msg"
+}
