f768d0d8719d396537515c1b97b138334a751032
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-07-12 15:47:34 +0200

    Update sklep.js

--- commands/sklep.js
+++ commands/sklep.js
@@ -5,22 +5,23 @@ const shop = require("../shop.json");
 module.exports.run = async (blaki, message, args) => {
   message.delete().catch(O_o=>{});
   Fortnite.FortniteStore('en', async (data) => {
-    data = JSON.parse(data);
-    let channel = blaki.channels.find('id', shop.channelid);
-    if(channel) {
-      if(channel.topic !== data['date']){
-        channel.setTopic(data['date']);
-        var list = [];
-        data['items'].forEach(async element => {
-          await list.push(element.item.images.information);
-        });
-        channel.send(shop.msg.replace(`{DATE}`, `${data['date']}`));
-        list.forEach(async element => {
-          await channel.send(element);
-        });
-      }
-    }
-  });
+        data = JSON.parse(data);
+        let channel = blaki.channels.find('id', shop.channelid);
+        if(channel) {
+          if(channel.topic !== data['date']){
+            channel.setTopic(data['date']);
+            var list = [];
+            data['items'].forEach(async element => {
+              await list.push(element.item.images.information);
+            });
+
+            channel.send(shop.msg.replace(`{DATE}`, `${data['date']}`));
+            list.forEach(async element => {
+              await channel.sendFile(element);
+            });
+          }
+        }
+      });
 }
 
 module.exports.help = {
