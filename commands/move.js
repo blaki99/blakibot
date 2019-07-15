f6d41eee1ca721696b18e31608b7b408363b8e19
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-07-15 04:04:50 +0200

    Update move.js

--- commands/move.js
+++ commands/move.js
@@ -22,7 +22,7 @@ module.exports.run = async (blaki, message, args) => {
 		return;
 	}
 
-	var toChannel = message.guild.channels.get(args[0].replace(/<#|>/g, ''));
+	let toChannel = message.guild.channels.get(args[0].replace(/<#|>/g, ''));
 	if (!toChannel) {
 		message.channel.send('Nie znaleziono oznaczonego kanału!');
 		return;
@@ -33,18 +33,18 @@ module.exports.run = async (blaki, message, args) => {
 		return;
 	}
 
-	var fromChannel = message.channel;
+	let fromChannel = message.channel;
 	if (args[2]) {
 		fromChannel = message.guild.channels.get(args[2].replace(/<#|>/g, ''));
 		if (!fromChannel || fromChannel.type !== 'text') fromChannel = message.channel;
 	}
 
-	var m = await message.channel.send('Moving message...');
+	let m = await message.channel.send('Moving message...');
 
 	fromChannel.messages.fetch(args[1]).then(async (message) => {
-		var wbs = await toChannel.fetchWebhooks();
-		if (wbs.size < 1) var wb = await toChannel.createWebhook('Move Message');
-		else var wb = wbs.first();
+		let wbs = await toChannel.fetchWebhooks();
+		if (wbs.size < 1) let wb = await toChannel.createWebhook('Move Message');
+		else let wb = wbs.first();
 
 		wb.send(message.content || '', { username: message.author.tag, avatarURL: message.author.avatarURL(), embeds: message.embeds, files: message.attachments.array() }).then(() => {
 		  m.edit('Moved message from user ' + Discord.Util.escapeMarkdownw(message.author.tag) + ' from ' + fromChannel.toString() + ' to ' + toChannel.toString());
