9f68e2db971cebeea57bc0172a404c3b169fc350
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-07-15 04:37:38 +0200

    Update move.js

--- commands/move.js
+++ commands/move.js
@@ -3,11 +3,11 @@ const config = require("../blakiconfig.json");
 
 module.exports.run = (blaki, message, args) => {
 
-
+    message.delete().catch(O_o=>{});
     if (message.member.roles.some(r=>[config.modrole].includes(r.name))) {
         let destchannel = message.mentions.channels.first();
         if (destchannel == null) {
-            message.channel.send('The channel could not be found or you have mentioned/entered an incorrect channel.')
+            message.channel.send('Kanał nie został znaleziony lub wprowadziłeś błędną nazwe kanału!')
             return;
         }
 
@@ -16,36 +16,38 @@ module.exports.run = (blaki, message, args) => {
             let messageEmbed = new Discord.RichEmbed()
             if (orig_message.attachments.array(0).length !== 0  ) {
                messageEmbed
-                   .setTitle(`Moved message from: #${orig_message.channel.name}`)
+                   .setColor("#FF0000")
+                   .setTitle(`Wiadomość przeniesiona z: #${orig_message.channel.name}`)
                    .setThumbnail(orig_message.author.avatarURL)
                    .setAuthor(orig_message.author.username)
-                   .addField("Original Message", orig_message.attachments.first().proxyURL)
+                   .addField("**Oryginalna Wiadomość:**", orig_message.attachments.first().proxyURL)
                 if (orig_message.attachments.first().filename.endsWith('.jpg') || orig_message.attachments.first().filename.endsWith('.png')) {
                     messageEmbed
                         .setImage(orig_message.attachments.first().proxyURL)
                 }
             } else {
                 messageEmbed
-                    .setTitle(`Moved message from: #${orig_message.channel.name}`)
+                    .setColor("#FF0000")
+                    .setTitle(`Wiadomość przeniesiona z: #${orig_message.channel.name}`)
                     .setThumbnail(orig_message.author.url)
                     .setAuthor(orig_message.author.username)
-                    .addField("Original Message", orig_message.content)
+                    .addField("**Oryginalna Wiadomość:**", orig_message.content)
             }
 
 
 
-            message.channel.send(`This message has been moved to ${destchannel}`)
+            message.channel.send(`Wiadomość została przeniesiona na ${destchannel}`)
             destchannel.send(messageEmbed);
             orig_message.delete()
         }).catch(err => {
-            if (err.message == "Unknown Message"){
-                message.channel.send("The message could not be found.")
+            if (err.message == "Nieznana Wiadomość."){
+                message.channel.send("Nie można znaleźć wiadomości!")
             } else {
                 console.log(err)
             }
         })
     } else {
-        message.channel.send("You do not have permission to execute this command.")
+        message.channel.send("Nie masz uprawnień do użycia tej komendy!")
     }
 
 };
