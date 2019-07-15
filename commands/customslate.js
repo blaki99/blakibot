98c6f23691b969965684630b8a6c14cb8326361f
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-07-16 01:08:43 +0200

    Update customslate.js

--- commands/customslate.js
+++ commands/customslate.js
@@ -6,6 +6,7 @@ module.exports.run = async (blaki, message, args) => {
 
     let pass = (args[0]);
     let mode = args.slice(1).join(' ')
+    const ping = "<@&535089879420502017>"
     const zasady0 = "**» NIE** WALCZ DOPÓKI NIE ZAMKNIE SIĘ 2 STREFA!"
     const zasady1 = "**» NIE** UŻYWAJ TRYBU ANONIMOWEGO PODCZAS GRY!"
     const zasady2 = "**» NIE** LATAJ NA SUNNY STEPS KIEDY GRA BLAKI"
@@ -29,7 +30,7 @@ module.exports.run = async (blaki, message, args) => {
     .addField("**ZASADY:**", zasady0 + `\n` +zasady1 + `\n`+ zasady2 + `\n`+ zasady3 + `\n`+ zasady4)
     .setTimestamp(message.createdAt)
     .setFooter("Kliknij reakcje jeśli grasz", "https://i.imgur.com/cgF1hsE.png");
-    message.channel.send(`@everyone`);
+    message.channel.send(ping);
     message.channel.send(customEmbed).then(function (message) {
         message.react(check)
     })
