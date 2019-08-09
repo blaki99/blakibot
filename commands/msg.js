5965176473ca271bc5aecd718b601b7064ed2ed5
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-08-09 23:31:52 +0200

    Update msg.js

--- commands/msg.js
+++ commands/msg.js
@@ -15,7 +15,8 @@ module.exports.run = async (blaki, message, args) => {
     message.delete();
     let msgEmbed = new Discord.RichEmbed()
     .setColor("#ffa500")
-    .addField("__**INFORMACJA:**__", `${ramka}${msg} ${msg2}${ramka2}`, true)
+    .addField("__**INFORMACJA:**__", `${ramka}
+${msg} ${msg2}${ramka2}`, true)
     .addField("__**OD:**__", `${message.author}`, true)
     .setTimestamp(message.createdAt)
     .setFooter("Kliknij reakcje jeśli przeczytałeś", "https://i.imgur.com/3Q7TQyy.png");
