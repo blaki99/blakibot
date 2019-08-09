98ac7c4edc7214c8a5f1d66978b7fb9648247f7f
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-08-09 23:37:59 +0200

    Update kod.js

--- commands/kod.js
+++ commands/kod.js
@@ -5,10 +5,10 @@ module.exports.run = async (blaki, message, args) => {
     message.delete().catch(O_o=>{});
     let serverembed = new Discord.RichEmbed()
     .setDescription(`**KOD W SKLEPIE!**`)
-    .setColor("#18a6e8")
+    .setColor("#ffa500")
     .setImage('https://i.imgur.com/jj2M5mv.png')
     .setTimestamp(message.createdAt)
-    .setFooter('Wspieraj Najlepszego Twórcę!', 'https://i.imgur.com/mNBIfzO.png');
+    .setFooter('Wspieraj Najlepszego Twórcę!', 'https://i.imgur.com/3Q7TQyy.png');
 
     message.channel.send(serverembed);
 }
