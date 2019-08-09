cef339f0844813bbb2ba2c30cefcaf14f88f3863
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-08-09 21:17:54 +0200

    Update solo.js

--- commands/solo.js
+++ commands/solo.js
@@ -7,11 +7,11 @@ module.exports.run = async (blaki, message, args) => {
     let pass = (args[0]);
     let game = args.slice(1).join(' ')
     let everyone = message.guild.defaultRole;
-    const zasady0 = "**» NIE** WALCZ DOPÓKI NIE ZAMKNIE SIĘ 2 STREFA!"
-    const zasady1 = "**» NIE** WALCZ PRZED 3 STREFĄ JEŚLI WŁĄCZY SIĘ STORM SURGE!"
-    const zasady2 = "**» NIE** LATAJ NA SUNNY STEPS KIEDY GRA BLAKI!"
-    const zasady3 = "**» NIE** UŻYWAJ TRYBU ANONIMOWEGO PODCZAS GRY!"
-    const zasady4 = "**» NIE** BIJEMY SIĘ NA PIERWSZYCH MIEJSCÓWKACH !"
+    const zasady0 = "**» NIE** UŻYWAJ ROBOTÓW PODCZAS GRY !"
+    const zasady1 = "**» NIE** UDOSTĘPNIAJ NIKOMU HASŁA DO GRY !"
+    const zasady2 = "**» NIE** STREAMSNIPUJ INNYCH GRACZY !"
+    const zasady3 = "**» NIE** UŻYWAJ TRYBU ANONIMOWEGO PODCZAS GRY !"
+    const zasady4 = "**NIESTOSOWANIE SIĘ DO POWYŻSZYCH ZASAD GROZI BANEM !"
     const react = '609463766681255936'
   
     if(!message.member.roles.has(HOST.id)) return message.reply("Ooops, nie posiadasz uprawnień!");
@@ -26,7 +26,7 @@ module.exports.run = async (blaki, message, args) => {
     .setURL('https://discord.gg/u9W8euF')
     .addField("**HASŁO:**", `**${pass}**`, true)
     .addField("**HOST:**", `${message.author}`, true)
-    .addField("**ZASADY:**", zasady0 + `\n` +zasady1 + `\n`+ zasady2 + `\n`+ zasady3 + `\n`+ zasady4)
+    .addField("**ZASADY:**", zasady0 + `\n` +zasady1 + `\n`+ zasady2 + `\n`+ zasady3 + `\n`+ `n`+ zasady4)
     .setTimestamp(message.createdAt)
     .setFooter("Kliknij reakcje jeśli grasz", "https://i.imgur.com/etQAdfG.png");
     message.channel.send(everyone.toString());
