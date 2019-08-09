50afa66d332fc6ce5b7f25bf0e4f5f5b083da9c8
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-08-10 00:20:13 +0200

    Update customslate.js

--- commands/customslate.js
+++ commands/customslate.js
@@ -25,9 +25,9 @@ module.exports.run = async (blaki, message, args) => {
     .setThumbnail('https://i.imgur.com/rmxBcdK.png')
     .setTitle(`**${mode} CUSTOM**`)
     .setURL('https://discord.gg/u9W8euF')
-    .addBlankField()
     .addField("**HASŁO:**", `**${pass}**`, true)
     .addField("**HOST:**", `${message.author}`, true)
+    .addBlankField()
     .addField("**ZASADY:**", zasady0 + `\n` +zasady1 + `\n`+ zasady2 + `\n`+ zasady3 + `\n`+ zasady4)
     .setTimestamp(message.createdAt)
     .setFooter("Kliknij reakcje jeśli grasz", "https://i.imgur.com/3Q7TQyy.png");
