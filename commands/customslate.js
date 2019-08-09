d3264f2bfddfd19885f6f0b6858618e991300b16
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-08-10 00:14:44 +0200

    Update customslate.js

--- commands/customslate.js
+++ commands/customslate.js
@@ -9,10 +9,10 @@ module.exports.run = async (blaki, message, args) => {
     let everyone = message.guild.defaultRole;
     const zasady0 = "**» NIE** WALCZ DOPÓKI NIE ZAMKNIE SIĘ 2 STREFA!"
     const zasady1 = "**» NIE** WALCZ PRZED 3 STREFĄ JEŚLI WŁĄCZY SIĘ STORM SURGE!"
-    const zasady2 = "**» NIE** LATAJ NA SUNNY STEPS KIEDY GRA BLAKI!"
+    const zasady2 = "**» NIE** UŻYWAJ ROBOTÓW PODCZAS GRY!"
     const zasady3 = "**» NIE** UŻYWAJ TRYBU ANONIMOWEGO PODCZAS GRY!"
     const zasady4 = "**» NIE** BIJEMY SIĘ NA PIERWSZYCH MIEJSCÓWKACH !"
-    const check = '601185797097652224'
+    const check = '609497709593755668'
 
     if(!message.member.roles.has(HOST.id)) return message.reply("Ooops, nie posiadasz uprawnień!");
     if(!args[0]) return message.channel.send("❌ _Wprowadź prawidłowe wartości, **!late hasło tryb**_ ❌").then(() =>
@@ -21,15 +21,15 @@ module.exports.run = async (blaki, message, args) => {
     })
     message.delete();
     let customEmbed = new Discord.RichEmbed()
-    .setColor("#ff005c")
+    .setColor("#ffa500")
     .setThumbnail('https://i.imgur.com/rmxBcdK.png')
-    .setTitle("__**NOWA GRA!**__")
+    .setTitle(`**${mode} GAME**`)
+    .setURL('https://discord.gg/u9W8euF')
     .addField("**HASŁO:**", `**${pass}**`, true)
     .addField("**HOST:**", `${message.author}`, true)
-    .addField("**TRYB:**", `**${mode}**`)
     .addField("**ZASADY:**", zasady0 + `\n` +zasady1 + `\n`+ zasady2 + `\n`+ zasady3 + `\n`+ zasady4)
     .setTimestamp(message.createdAt)
-    .setFooter("Kliknij reakcje jeśli grasz", "https://i.imgur.com/mNBIfzO.png");
+    .setFooter("Kliknij reakcje jeśli grasz", "https://i.imgur.com/3Q7TQyy.png");
     message.channel.send(everyone.toString());
     message.channel.send(customEmbed).then(function (message) {
         message.react(check)
