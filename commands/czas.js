85e807c21805f6192ba182b45eaf619968d61814
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-08-11 12:05:38 +0200

    Create czas.js

---
+++ commands/czas.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+var schedule = require('node-schedule');
+
+module.exports.run = async (blaki, message, args) => {
+
+    var rule = new schedule.RecurrenceRule();
+    rule.hour= 12;
+    rule.minute= 10;
+    const x_channel = blaki.channels.get("535153796129619969");
+    var j = schedule.scheduleJob(rule, function(){
+      let serverembed = new Discord.RichEmbed()
+    .setDescription(`**WPISZ KOD W SKLEPIE!**`)
+    .setColor("#ffa500")
+    .setImage('https://i.imgur.com/jj2M5mv.png')
+    .setTimestamp(message.createdAt)
+    .setFooter('Wspieraj Najlepszego Twórcę!', 'https://i.imgur.com/3Q7TQyy.png');
+
+    message.channel.send(serverembed);
+    });
+}
+
+module.exports.help = {
+    name:"start"
+}
