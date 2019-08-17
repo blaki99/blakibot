d6e850f34b6ade0cece4dfbf3ebb25d4bafd8315
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-08-17 23:36:02 +0200

    Create trio.js

---
+++ commands/trio.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (blaki, message, args) => {
+    
+    message.delete().catch(O_o=>{});
+    let TrioEmbed = new Discord.RichEmbed()
+    .setColor("#FF0000")
+    .addField("**TRIO:**", "• BlaKi" + `\n` + "• Matek" + `\n` + "• kogutzz")
+    .setTimestamp(message.createdAt)
+    .setImage('https://i.imgur.com/DBQt3ci.jpg')
+    .setFooter("Kiedyś będziemy TOP #1", "https://i.imgur.com/3Q7TQyy.png");
+
+    message.channel.send(TrioEmbed);
+}
+
+module.exports.help = {
+  name:"trio"
+}
