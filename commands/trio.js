07cdbaa53e8ed2384266116858203f87c78ba30e
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-08-17 23:42:50 +0200

    Update trio.js

--- commands/trio.js
+++ commands/trio.js
@@ -5,7 +5,7 @@ module.exports.run = async (blaki, message, args) => {
     message.delete().catch(O_o=>{});
     let TrioEmbed = new Discord.RichEmbed()
     .setColor("#FF0000")
-    .addField("_**TRIO:**_", "• BlaKi" + `\n` + "• Matek" + `\n` + "• Kogutzz")
+    .addField("__**TRIO:**__", "• BlaKi" + `\n` + "• Matek" + `\n` + "• Kogutzz")
     .setTimestamp(message.createdAt)
     .setImage('https://i.imgur.com/DBQt3ci.jpg')
     .setFooter("Kiedyś będziemy TOP #1", "https://i.imgur.com/3Q7TQyy.png");
