8d282d01c1eb1eae9acf0bfa209956455e6d3e8c
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-08-22 15:14:03 +0200

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -3,19 +3,31 @@ const Discord = require("discord.js");
 module.exports.run = async (blaki, message, args) => {
 
     if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("❌ **Nie można wykonać akcji** ❌");
-   
     if(args[0] == "help"){
       message.reply("Użycie: !unban <user> <reason>");
       return;
     }
-    
     let bUser = await blaki.fetchUser(args[0]);
     if(!bUser) return message.channel.send("❌ **Nie znaleziono użytkownika** ❌");
     let bReason = args.join(" ").slice(22);
     if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("❌ **Nie można zbanować tego użytkownika** ❌");
     
-    message.delete();
+    message.delete().catch(O_o=>{});
     
+    try{
+        let DMunbanEmbed = new Discord.RichEmbed()
+        .setColor("#ffa500")
+        .setTitle("__**ZOSTAŁEŚ ODBANOWANY**__")
+        .addField("Serwer", `**${message.guild.name}**`)
+        .addField("Odbanowany Przez", `<@${message.author.id}>`)
+        .addField("Powód", bReason)
+        .setTimestamp(message.createdAt)
+        .setFooter('Zostałeś Odbanowany!', 'https://i.imgur.com/3Q7TQyy.png');
+        await bUser.send(DMunbanEmbed)
+    }catch(e){
+        message.channel.send(`Użytkownik został zbanowany na **BlaKi's Discord** ale niestemy ma zablokowane wiadomości prywatne.`)
+    }
+
     let unbanEmbed = new Discord.RichEmbed()
     .setColor("#ffa500")
     .addField("Odbanowany Użytkownik", `${bUser}`)
@@ -27,13 +39,9 @@ module.exports.run = async (blaki, message, args) => {
 
     let incidentchannel = message.guild.channels.find(`name`, "📕  »  ᴅᴢɪᴇɴɴɪᴋ  ᴢᴅᴀʀᴢᴇɴ");
     if(!incidentchannel) return message.channel.send("❌ **Proszę utworzyć kanał zdarzeń** ❌");
-    
-    try {
-      message.guild.unban(bUser, {bReason: bReason});
-      incidentchannel.send(unbanEmbed);
-    }catch(e){
-        message.channel.send("Nie można wykonać akcji")
-    }
+
+    message.guild.unban(bUser, {bReason: bReason});
+    incidentchannel.send(unbanEmbed);
 }
 
 module.exports.help = {
