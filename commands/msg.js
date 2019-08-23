f1fba527390714f87c84af52da58a293592c3bc4
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-08-23 12:03:33 +0200

    Update msg.js

--- commands/msg.js
+++ commands/msg.js
@@ -9,7 +9,6 @@ module.exports.run = async (blaki, message, args) => {
     let msg2 = args.slice(1).join(' ')
     const ramka = "```"
     const ramka2 = "```"
-    const check = '609497709593755668'
 
     if(!message.member.roles.has(host.id)) return message.reply("Ooops, nie posiadasz uprawnień!");
     if(!args[0]) return message.channel.send("❌ _Wprowadź prawidłowe wartości, **!msg <wiadomość>**_ ❌")
@@ -20,7 +19,7 @@ module.exports.run = async (blaki, message, args) => {
 ${msg} ${msg2}${ramka2}`)
     .addField("__**OD:**__", `${message.author}`)
     .setTimestamp(message.createdAt)
-    .setFooter("Kliknij reakcje jeśli przeczytałeś", `${config.avatar}`);
+    .setFooter("Nowa Informacja", `${config.avatar}`);
     message.channel.send(msgEmbed).then(function (message) {
         message.react(check)
     })
