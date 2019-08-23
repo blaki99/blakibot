7e90e1024dac697d3bf3d6898b8cac806d90a99b
blaki99 <50325086+blaki99@users.noreply.github.com>
2019-08-23 15:45:50 +0200

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 
-module.exports.run = async (custom, message, args) => {
+module.exports.run = async (blaki, message, args) => {
 
   if(!message.member.hasPermission("MANAGE_SERVER")) return message.reply("Nie posiadasz uprawnień.");
   if(!args[0] || args[0 == "help"]) return message.reply("Użycie: -prefix <nowy prefix>");
